3263f8fd95d35b7d00055831bdf8cc3423cf672b
Miyanari61 <mynr_yuk@icloud.com>
2020-03-14 14:34:57 +0900

    リファクタリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only:[:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.order(created_at: :desc)
@@ -17,14 +18,11 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: "投稿を更新しました。"
     else
@@ -33,7 +31,6 @@ class PostsController < ApplicationController
   end
   
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: "投稿を削除しました。"
   end
@@ -41,6 +38,10 @@ class PostsController < ApplicationController
   private
   def post_params # ストロングパラメータを定義する
     params.require(:post).permit(:caption, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
