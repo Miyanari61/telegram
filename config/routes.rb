b89e56f139865ec1a4275832adf927da02cf1bc3
Miyanari61 <mynr_yuk@icloud.com>
2020-03-15 14:50:12 +0900

    ユーザー照会ページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
     devise_for :users
     resources :posts
     resources :comments, only: [ :create, :destroy ]
+    resources :users, only: :show
     get 'comments' => redirect('posts')
     root 'posts#index'
   end
